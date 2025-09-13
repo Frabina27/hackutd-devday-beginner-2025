6cdb6e2eb66f5bd65ac2004faef88a2e9a463527
Snowie <frabina2016@gmail.com>
2025-09-13 16:30:14 -0500

    hello

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
+    <div className="w-full bg-purple-100 py-16 px-8 border-b border-gray-300">
+        <div className="max-w-4xl mx-auto flex justify-center">
+          <div className="text-left">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl text-gray-800 leading-0.5">
+              Maxing our GPA <span className="text-black font-bold">GPA</span>
+            </h1>
 
+            <h2 className="text-4xl md:text-5xl lg:text-6xl text-gray-800 mt-2">
+              One <span className="text-black font-bold">Class</span> at a Time.
+            </h2>
+
+          </div>
+        </div>
     </div>
   );
 }
